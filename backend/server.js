c52629a01256a10e556d14d5e6c6c25e8d9b160d
iamsayannath <sayannath441@gmail.com>
2025-08-08 19:42:03 +0530

    add cors

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,14 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(
+  {
+    origin: 'https://mern-authentication-frontend-spgp.onrender.com',
+    credentials: true
+  }
+));
 
-// {
-//   origin: 'http://localhost:5173',
-//     credentials: true;
-// }
+
 
 app.use('/api/auth', authRoutes);
 
