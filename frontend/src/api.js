9a991e87592f292b9d80e9dba1a4891a5a00543b
iamsayannath <sayannath441@gmail.com>
2025-08-08 19:34:24 +0530

    add backend api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mern-authentication-backend-ma24.onrender.com/api",
   withCredentials: true,
 });
